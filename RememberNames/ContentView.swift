f44a2c5d155921264d99a209a7800ac4a87dbac4
ricardonovelot <ricardonovelo97.tools@gmail.com>
2024-05-18 13:11:53 -0600

    Initial Commit

---
+++ RememberNames/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RememberNames
+//
+//  Created by Ricardo on 18/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RememberNames/RememberNamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RememberNamesApp.swift
+//  RememberNames
+//
+//  Created by Ricardo on 18/05/24.
+//
+
+import SwiftUI
+
+@main
+struct RememberNamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
